da3bac3191da6864783c24d0ee27a614760835a3
Hillel Garcia <hgarcia@newrelic.com>
2023-09-12 10:09:19 +0300

    Handling errors and response

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -6,27 +6,37 @@ import type { NextApiRequest, NextApiResponse } from "next";
 const SLACK_POST_MESSAGE_URL = "https://slack.com/api/chat.postMessage";
 const CHANNEL_ID = "C05SEUQ1532";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ challenge: req.body.challenge || ":(" });
-  let response_text = "I don't understand that command. Try again?";
-  const { event } = req.body;
-  console.log("Event: " + JSON.stringify(event));
-  console.log("Token: " + process.env.BOT_TOKEN);
+export default async function bhandler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { event } = req.body;
+    let response_text = "I don't understand that command. Try again?";
 
-  switch (event.type) {
-    // Message events that mention the bot (e. @savior)
-    case "app_mention":
-      fetch(SLACK_POST_MESSAGE_URL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.BOT_TOKEN}`,
-        },
-        body: JSON.stringify({
-          channel: CHANNEL_ID,
-          text: response_text
-        }),
-      });
-    break;
+    switch (event.type) {
+      // Message events that mention the bot (e.g., @savior)
+      case "app_mention":
+        const response = await fetch(SLACK_POST_MESSAGE_URL, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.BOT_TOKEN}`,
+          },
+          body: JSON.stringify({
+            channel: CHANNEL_ID,
+            text: response_text,
+          }),
+        });
+
+        if (response.ok) {
+          res.status(200).json({ message: "Message sent successfully" });
+        } else {
+          res.status(500).json({ message: "Failed to send message to Slack" });
+        }
+        break;
+      default:
+        res.status(200).json({ challenge: req.body.challenge || ":(" });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 }
