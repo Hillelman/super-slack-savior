78e13be08f5274bd17d094b7e9a546819075d756
Hillel Garcia <hgarcia@newrelic.com>
2023-09-12 11:35:32 +0300

    Adding stop request flag

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -30,25 +30,22 @@ async function fetchData(query: string) {
     console.error('Error fetching data:', error);
   }
 }
-let stopRequest = false;
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   // res.status(200).send("Hello, world!");
-  if(stopRequest) {
-    return
-  }
+
   try {
     const { event } = req.body;
 
     switch (event.type) {
       // Message events that mention the bot (e.g., @savior)
       case "app_mention":
-        stopRequest = true;
+       
         const text = await fetchData(event.text);
-        stopRequest = false;
-        if (!text) {
-          res.status(200).json({ message: "No message to send" });
-          return;
-        }
+        
+        // if (!text) {
+        //   res.status(200).json({ message: "No message to send" });
+        //   return;
+        // }
 
         const response = await fetch(SLACK_POST_MESSAGE_URL, {
           method: "POST",
